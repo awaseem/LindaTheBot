d47c70561baa88971d9b643166ba36fdeb349685
Ali Waseem <waseema393@gmail.com>
2016-09-30 22:29:43 -0700

    abstracted telegram updates

--- bot.go
+++ bot.go
@@ -4,12 +4,8 @@ import (
 	"log"
 
 	"github.com/awaseem/LindaTheBot/helpers"
-	"github.com/awaseem/LindaTheBot/interactions"
 	"github.com/awaseem/LindaTheBot/store"
-	"github.com/go-telegram-bot-api/telegram-bot-api"
 )
-
-const telegramBotKey = "TELEGRAM_KEY"
 
 func main() {
 	// Create store for holding messages
@@ -17,28 +13,7 @@ func main() {
 	if storeErr != nil {
 		log.Panic(storeErr)
 	}
-	// Setup telegram polling for updates
-	bot, err := tgbotapi.NewBotAPI(helpers.GetEnvOrElse(telegramBotKey, ""))
-	if err != nil {
-		log.Panic(err)
-	}
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-	updates, err := bot.GetUpdatesChan(u)
+	updates := helpers.StartChannel()
 	// Handle updates for anything that happens within the chat
-	for update := range updates {
-		// continue on any messages that are pictures
-		if update.Message == nil || update.Message.Text == "" {
-			continue
-		}
-		saveErr := store.Save(update.Message.From.FirstName, update.Message.From.LastName, update.Message.From.UserName, update.Message.Text, update.Message.Time())
-		// Any errors that occur while saving, just ignore them!
-		if saveErr != nil {
-			log.Printf("Error: Failed to save message!")
-		}
-		// Send any interactions that are handled
-		if response := interactions.HandleMessage(update.Message.Text, *update.Message.From); response != "" {
-			bot.Send(helpers.ResponseMessage(update.Message.Chat.ID, response, update.Message.MessageID))
-		}
-	}
+	helpers.HandleUpdates(updates)
 }
--- helpers/telegramHelpers.go
+++ helpers/telegramHelpers.go
@@ -1,10 +1,54 @@
 package helpers
 
-import "github.com/go-telegram-bot-api/telegram-bot-api"
+import (
+	"log"
+
+	"github.com/awaseem/LindaTheBot/interactions"
+	"github.com/awaseem/LindaTheBot/store"
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+const telegramBotKey = "TELEGRAM_KEY"
+
+var botAPI *tgbotapi.BotAPI
+
+// StartChannel start the channel
+func StartChannel() <-chan tgbotapi.Update {
+	// Setup telegram polling for updates
+	bot, err := tgbotapi.NewBotAPI(GetEnvOrElse(telegramBotKey, ""))
+	if err != nil {
+		log.Panic(err)
+	} else {
+		botAPI = bot
+	}
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+	updates, err := bot.GetUpdatesChan(u)
+	return updates
+}
 
 // ResponseMessage create telegram response message
 func ResponseMessage(chatID int64, message string, messageID int) tgbotapi.MessageConfig {
 	msg := tgbotapi.NewMessage(chatID, message)
 	msg.ReplyToMessageID = messageID
 	return msg
+}
+
+// HandleUpdates update channels
+func HandleUpdates(updates <-chan tgbotapi.Update) {
+	for update := range updates {
+		// continue on any messages that are pictures
+		if update.Message == nil || update.Message.Text == "" {
+			continue
+		}
+		saveErr := store.Save(update.Message.From.FirstName, update.Message.From.LastName, update.Message.From.UserName, update.Message.Text, update.Message.Time())
+		// Any errors that occur while saving, just ignore them!
+		if saveErr != nil {
+			log.Printf("Error: Failed to save message!")
+		}
+		// Send any interactions that are handled
+		if response := interactions.HandleMessage(update.Message.Text, *update.Message.From); response != "" {
+			botAPI.Send(ResponseMessage(update.Message.Chat.ID, response, update.Message.MessageID))
+		}
+	}
 }
